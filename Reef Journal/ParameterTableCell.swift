caa97bf83d502768858be4c7a43f5516170e84be
Christopher Harding <christopher.harding@me.com>
2014-07-02 10:50:09 -0600

    Added a custom class for managing the table view cells.

---
+++ Reef Journal/ParameterTableCell.swift
@@ -0,0 +1,14 @@
+//
+//  ParameterCellViewController.swift
+//  Reef Journal
+//
+//  Created by Chris Harding on 7/2/14.
+//  Copyright (c) 2014 Christopher Harding. All rights reserved.
+//
+
+import UIKit
+
+class ParameterTableCell: UITableViewCell {
+
+    
+}
