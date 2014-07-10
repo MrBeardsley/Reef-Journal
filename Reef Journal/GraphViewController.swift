9cf8e910b3b513e37c55d63d399fd0f9d7d0eafe
Christopher Harding <christopher.harding@me.com>
2014-07-10 16:25:36 -0600

    Added the view controller to show the graph of the parameters. Also added the file for the custom class to power the view.

---
+++ Reef Journal/GraphViewController.swift
@@ -0,0 +1,9 @@
+//
+//  GraphViewController.swift
+//  Reef Journal
+//
+//  Created by Chris Harding on 7/10/14.
+//  Copyright (c) 2014 Christopher Harding. All rights reserved.
+//
+
+import Foundation
