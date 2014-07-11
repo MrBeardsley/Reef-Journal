a39e1ec2562d528f0c76a87a5fa5a9277a01861e
Christopher Harding <christopher.harding@me.com>
2014-07-10 20:27:06 -0600

    Added the custom view to draw the graph. Filled in a dummy drawing with just a square.

---
+++ Reef Journal/GraphView.swift
@@ -0,0 +1,23 @@
+//
+//  GraphView.swift
+//  Reef Journal
+//
+//  Created by Chris Harding on 7/10/14.
+//  Copyright (c) 2014 Christopher Harding. All rights reserved.
+//
+
+import UIKit
+
+class GraphView: UIView {
+
+    override func drawRect(rect: CGRect) {
+
+        var context = UIGraphicsGetCurrentContext()
+        CGContextSetLineWidth(context, 4.0)
+        CGContextSetStrokeColorWithColor(context, UIColor.blueColor().CGColor)
+        let rectangle = CGRectMake(60,170,200,80)
+        CGContextAddRect(context, rectangle)
+        CGContextStrokePath(context)
+
+    }
+}
