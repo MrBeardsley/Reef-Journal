9c2ccd467105689f372d315aa99cf861410b30f5
Christopher Harding <chris@Chris-MacBook-Pro.local>
2014-10-01 20:07:02 -0600

    Moved functions into their own file and started putting documentation on the functions.

---
+++ Reef Journal/Functions.swift
@@ -0,0 +1,98 @@
+//
+//  Functions.swift
+//  Reef Journal
+//
+//  Created by Christopher Harding on 10/1/14.
+//  Copyright (c) 2014 Epic Kiwi Interactive. All rights reserved.
+//
+
+import Foundation
+
+/**
+Return the number of decimal places a parameter will display.
+
+:param: type The type of parameter
+
+:returns: The number of decimal places
+*/
+func decimalPlacesForParameter(type: Parameter) -> Int {
+    switch type {
+    case .Calcium, .Magnesium, .Strontium, .Potasium, .Phosphate, .Ammonia, .Nitrite, .Nitrate:
+        return 0
+    case .Temperature, .pH:
+        return 1
+    case .Alkalinity:
+        if let intValue = NSUserDefaults.standardUserDefaults().valueForKey(PreferenceIdentifier.AlkalinityUnits.rawValue) as? Int {
+            switch AlkalinityUnit(rawInt: intValue) {
+            case .DKH, .MeqL: return 1
+            case .PPT: return 0
+            }
+        }
+        else {
+            return 0
+        }
+    case .Salinity:
+        if let intValue = NSUserDefaults.standardUserDefaults().valueForKey(PreferenceIdentifier.SalinityUnits.rawValue) as? Int {
+            switch SalinityUnit(rawInt: intValue) {
+            case .SG: return 3
+            case .PPT: return 0
+            }
+        }
+        else {
+            return 3
+        }
+    }
+}
+
+/**
+Determines if a paramter uses decimal places or not
+
+:param: type The type of parameter
+
+:returns: True/false if the parameter type uses decimal places
+*/
+func parameterTypeDisplaysDecimal(type: Parameter) -> Bool {
+    if decimalPlacesForParameter(type) > 0 {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+/**
+Takes a parameter and returns a string representing the unit label for the type of the parameter
+
+:param: type The type of parameter
+
+:returns: The unit of measure label
+*/
+func unitLabelForParameterType(type: Parameter) -> String {
+    switch (type) {
+    case .Calcium, .Magnesium, .Strontium, .Potasium, .Phosphate, .Ammonia, .Nitrite, .Nitrate:
+        return "ppm"
+    case .Alkalinity:
+        if let intValue = NSUserDefaults.standardUserDefaults().valueForKey(PreferenceIdentifier.AlkalinityUnits.rawValue) as? Int {
+            return AlkalinityUnit(rawInt: intValue).rawValue
+        }
+        else {
+            return AlkalinityUnit.DKH.rawValue
+        }
+    case .Salinity:
+        if let intValue = NSUserDefaults.standardUserDefaults().valueForKey(PreferenceIdentifier.SalinityUnits.rawValue) as? Int {
+            return SalinityUnit(rawInt: intValue).rawValue
+        }
+        else {
+            return SalinityUnit.SG.rawValue
+        }
+    case .pH:
+        return ""
+    case .Temperature:
+        if let intValue = NSUserDefaults.standardUserDefaults().valueForKey(PreferenceIdentifier.TemperatureUnits.rawValue) as? Int {
+            return TemperatureUnit(rawInt: intValue).rawValue
+        }
+        else {
+            return TemperatureUnit.Fahrenheit.rawValue
+        }
+    }
+}
